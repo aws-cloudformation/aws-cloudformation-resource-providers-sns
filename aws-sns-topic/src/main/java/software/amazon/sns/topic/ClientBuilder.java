577f09963cace1d5ca2902bc0c7fa5f9c8d1b2ce
Ryan Lohan <ryanloha@amazon.com>
2020-04-14 12:14:21 -0700

    Initial commit for AWS::SNS::Topic using existing schema

---
+++ aws-sns-topic/src/main/java/software/amazon/sns/topic/ClientBuilder.java
@@ -0,0 +1,29 @@
+package software.amazon.sns.topic;
+
+import software.amazon.awssdk.core.SdkClient;
+// TODO: replace all usage of SdkClient with your service client type, e.g; YourServiceAsyncClient
+// import software.amazon.awssdk.services.yourservice.YourServiceAsyncClient;
+// import software.amazon.cloudformation.LambdaWrapper;
+
+public class ClientBuilder {
+  /*
+  TODO: uncomment the following, replacing YourServiceAsyncClient with your service client name
+  It is recommended to use static HTTP client so less memory is consumed
+  e.g. https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-logs/blob/master/aws-logs-loggroup/src/main/java/software/amazon/logs/loggroup/ClientBuilder.java#L9
+
+  private static class LazyHolder {
+    static final YourServiceAsyncClient SERVICE_CLIENT = YourServiceAsyncClient.builder()
+              .httpClient(LambdaWrapper.HTTP_CLIENT)
+              .build();
+  }
+
+  public static YourServiceAsyncClient getClient() {
+    return LazyHolder.SERVICE_CLIENT;
+  }
+  */
+
+  // TODO: remove this implementation once you have uncommented the above
+  public static SdkClient getClient() {
+    return null;
+  }
+}
---
+++ aws-sns-topic/src/main/java/software/amazon/sns/topic/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.sns.topic;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-sns-topic.json");
+    }
+}
