0b677402f22c7cc25d52359e769fb091e971fac6
Ryan Lohan <ryanloha@amazon.com>
2020-04-19 07:45:19 -0700

    Adds schema and client for AWS::SNS::Subscription

--- aws-sns-subscription/src/main/java/software/amazon/sns/subscription/Configuration.java
+++ aws-sns-subscription/src/main/java/software/amazon/sns/subscription/Configuration.java
@@ -1,8 +1,16 @@
 package software.amazon.sns.subscription;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 class Configuration extends BaseConfiguration {
 
     public Configuration() {
         super("aws-sns-subscription.json");
     }
+
+    public JSONObject resourceSchemaJSONObject() {
+        return new JSONObject(new JSONTokener(this.getClass().getClassLoader().getResourceAsStream(schemaFilename)));
+    }
+
 }
